6db6455dda5110db855653182c42626a454125ff
Matias Juntunen <matias.juntunen@gmail.com>
2013-09-13 21:28:30 +0300

    Added example for Uguu

---
+++ UguuFeature/UguuFeature/ISimo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimoBot
+{
+    interface IFeature
+    {
+        void Init(ISimo Simo);
+    }
+
+    interface ICommand
+    {
+        void Execute(IServer Server, IChannel Channel, string Sender, string Message);
+    }
+
+    interface IChannel
+    {
+        void Say(string Message);
+    }
+
+    interface IServer
+    {
+    }
+
+    interface ISimo
+    {
+        void RegisterCommand(string Command, ICommand Handler);
+    }
+}
