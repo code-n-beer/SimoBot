988cf77f20caa7072c8c1affb48cb70870e5d15c
Tsarpf <tsarpf@gmail.com>
2013-11-03 05:41:15 +0200

    Added irc dotnet library and started thinking about how to handle multiple servers/channels/etc

--- SimoBot/Client.cs
+++ SimoBot/Client.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IrcDotNet;
 
 namespace SimoBot
 {
     class Client
     {
+        private IrcUserRegistrationInfo regInfo;
+        IrcClient client;
+        public Client(string nick, string realName, string userName, string password)
+        {
+            regInfo = new IrcUserRegistrationInfo();
+            regInfo.NickName = nick;
+            regInfo.Password = password;
+            regInfo.RealName = realName;
+            regInfo.UserName = userName;
+            regInfo.UserModes = new char[] {};
+            client = new IrcClient();
+        }
     }
 }
 
 /*
-            Simo simo = new Simo("SimoBot", "SimoBot", "simobot", "fuckshit");
-            foreach (var feature in features)
-            {
-                feature.Init(simo);
-            }
+Simo simo = new Simo("SimoBot", "SimoBot", "simobot", "fuckshit");
+foreach (var feature in features)
+{
+    feature.Init(simo);
+}
 
-            simo.Channels = new string[] { "#tkt-cocknballs" };
-            simo.Connect("openirc.snt.utwente.nl");
+simo.Channels = new string[] { "#tkt-cocknballs" };
+simo.Connect("openirc.snt.utwente.nl");
 
-            while (simo.Connected) ;    
+while (simo.Connected) ;    
 */
