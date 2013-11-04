236cef0c3ca944ff559d7780a63576527052e862
Tsarpf <tsarpf@gmail.com>
2013-11-04 15:13:56 +0200

    Started configloader. Nothing too smart done yet

---
+++ SimoBot/ConfigLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimoBot
+{
+    class ConfigLoader
+    {
+        public ConfigLoader(string filename = "config.txt")
+        {
+            StreamReader reader = new StreamReader(filename);
+            string line = reader.ReadLine();
+
+            Dictionary<string, Dictionary<string, string>> configs = new Dictionary<string, Dictionary<string, string>>();
+
+            string channel = "";
+            string server = "";
+            while (line != null)
+            {
+                if (line.Contains("@"))
+                {
+                    channel = line.Split('@')[0];
+                    server = line.Split('@')[1];
+                    //aaarghh shit I'm too tired for this
+                }
+
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
