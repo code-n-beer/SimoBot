a61190f799a9cd236c7d3c277744009b5ca892ab
Tsarpf <tsarpf@gmail.com>
2013-11-09 08:17:54 +0200

    Added HelpFeature

---
+++ SimoBot/HelpFeature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimoBot
+{
+    class HelpFeature
+    {
+        EngineMessageHandlers features;
+        public void RegisterFeature(EngineMessageHandlers features)
+        {
+            this.features = features;
+            features.commands["help"] = Execute;
+        }
+
+        public void Initialize(Dictionary<string, Dictionary<string, string>> configs)
+        {
+        }
+
+        public void Execute(IrcDotNet.IrcClient Client, string Channel, IrcDotNet.IrcUser Sender, string Message)
+        {
+            string helpString = "";
+            foreach (string command in features.commands.Keys)
+            {
+                helpString += command + " ";
+            }
+
+            helpString = "Available commands: " + helpString;
+
+            Client.LocalUser.SendMessage(Channel, helpString);
+        }
+    }
+}
