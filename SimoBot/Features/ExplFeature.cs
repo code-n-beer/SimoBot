4ccfca928c0051ebe71c7c5b2b072cdf10c8e9bc
Kviiri <kalle.viiri@gmail.com>
2013-11-17 20:52:38 +0200

    yet yet another off by one

--- SimoBot/Features/ExplFeature.cs
+++ SimoBot/Features/ExplFeature.cs
@@ -126,7 +126,7 @@ namespace SimoBot
         private int getNumericSuffix(string name)
         {
             int i = name.Length;
-            while(Char.IsDigit(name[--i]) && i >= 0);
+            while(Char.IsDigit(name[--i]) && i > 0);
             if(i+1 == name.Length)
             {
 		return -1;
