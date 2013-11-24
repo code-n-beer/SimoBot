9ccda40cd8334874f01b233f1d54b34921ce5f70
Sami Saada <sami.saada@gmail.com>
2013-11-24 13:58:49 +0200

    First attempt with timers

--- SimoBot/Features/TimerFeature.cs
+++ SimoBot/Features/TimerFeature.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace SimoBot
 {
@@ -15,17 +16,36 @@ namespace SimoBot
         {
             int _Delay;
             string _Message;
+            IrcClient _Client;
+            string _Channel;
+            string _Nick;
             
-            public SimoTimer(IrcClient Client, string Channel, int Delay, string Message)
+            public SimoTimer(IrcClient Client, string Channel, string Nick, int Delay, string Message)
             {
+                _Client = Client;
+                _Channel = Channel;
+                _Nick = Nick;
                 _Delay = Delay;
                 _Message = Message;
-                Client.LocalUser.SendMessage(Channel, TimerMessage());
+                System.Timers.Timer aTimer = new System.Timers.Timer(_Delay);
+                aTimer.Elapsed += new ElapsedEventHandler(TimerFiring);
+                aTimer.Enabled = true;
+                IrcSay(TimerMessage());
             }
             
             private string TimerMessage()
             {
                 return String.Format("Timer set {0} {1}", _Delay, _Message);
+            }
+            
+            private void TimerFiring()
+            {
+                IrcSay(String.Format("{0}: {1}", _Nick, _Message));
+            }
+            
+            private void IrcSay(string Message)
+            {
+                _Client.LocalUser.SendMessage(_Channel, Message);
             }
         }
 
@@ -40,7 +60,7 @@ namespace SimoBot
         public void Execute(IrcClient Client, string Channel, IrcUser Sender, string Message)
         {
             string[] splits = Message.Split(new char[] { ' ' }, 2, StringSplitOptions.RemoveEmptyEntries);
-            new SimoTimer(Client, Channel, int.Parse(splits[0]), splits[1]);
+            new SimoTimer(Client, Channel, Sender.NickName, int.Parse(splits[0]), splits[1]);
         }
     }
 }
