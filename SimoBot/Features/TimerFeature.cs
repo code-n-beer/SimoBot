049f538ef74c3b26e3d004a07d3d1a31177b8e38
Sami Saada <sami.saada@gmail.com>
2013-11-24 04:33:27 +0200

    Create TimerFeature.cs

---
+++ SimoBot/Features/TimerFeature.cs
@@ -0,0 +1,17 @@
+namespace SimoBot
+{
+    class TimerFeature : IFeature
+    {
+
+        public void RegisterFeature(EngineMessageHandlers features)
+        {
+        }
+
+        public void Initialize(Dictionary<string, Dictionary<string, string>> configs)
+        {
+        }
+        public void Execute(IrcClient Client, string Channel, IrcUser Sender, string Message)
+        {
+        }
+    }
+}
