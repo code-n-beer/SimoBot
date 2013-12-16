8f0344a3d97ab8dd9b20f3a7343d6fe1f6d39d24
Teemu Sarapisto <tsarpf@gmail.com>
2013-12-16 18:13:35 +0200

    Testing & debugging markov

---
+++ SimoBot/Features/MarkovTestFeature.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimoBot
+{
+	class MarkovTestFeature : IFeature
+	{
+		EngineMessageHandlers features;
+		public void RegisterFeature(EngineMessageHandlers features)
+		{
+			this.features = features;
+			features.commands["markov"] = Execute;
+		}
+
+		public void Initialize(Dictionary<string, Dictionary<string, string>> configs)
+		{
+		}
+
+		public void Execute(IrcDotNet.IrcClient Client, string Channel, IrcDotNet.IrcUser Sender, string Message)
+		{
+		}
+	}
+}
