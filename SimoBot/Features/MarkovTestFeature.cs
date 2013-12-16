f0c5f60e13fa7ed9a1e67d6dd2c530b6ad243fd4
Teemu Sarapisto <tsarpf@gmail.com>
2013-12-16 18:18:13 +0200

    testing adding a using statement

--- SimoBot/Features/MarkovTestFeature.cs
+++ SimoBot/Features/MarkovTestFeature.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceStack.Redis;
 
 namespace SimoBot
 {
 	class MarkovTestFeature : IFeature
 	{
+
 		EngineMessageHandlers features;
 		public void RegisterFeature(EngineMessageHandlers features)
 		{
@@ -21,6 +23,7 @@ namespace SimoBot
 
 		public void Execute(IrcDotNet.IrcClient Client, string Channel, IrcDotNet.IrcUser Sender, string Message)
 		{
+
 		}
 	}
 }
