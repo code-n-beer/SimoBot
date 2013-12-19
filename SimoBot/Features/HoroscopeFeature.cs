68d628c7d2a0c6aa96d074f7430a97ebabefd730
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2013-12-19 13:21:35 +0200

    Switched to using raw html parsing

--- SimoBot/Features/HoroscopeFeature.cs
+++ SimoBot/Features/HoroscopeFeature.cs
@@ -7,6 +7,7 @@ using IrcDotNet;
 using System.Xml;
 using System.Net;
 using System.Compat.Web;
+using System.Text.RegularExpressions;
 
 namespace SimoBot
 {
@@ -44,45 +45,75 @@ namespace SimoBot
             }
             else
             {
+
                 if (!signs.ContainsKey(message))
                 {
                     Client.LocalUser.SendMessage(channel, "Why won't you give me a sign~");
                     return;
                 }
-                XmlDocument xml = getXml(signs[message]);
-                int dayOfWeek = getDayOfWeek();
-                int magic1 = 1 + 2 * dayOfWeek;
-                var days = xml.DocumentElement.GetElementsByTagName("p")[magic1];
 
-                if (days == null || days.InnerText.Length < 20)
-                {
-                    Client.LocalUser.SendMessage(channel, "Misunderstood HTML");
-                    return;
-                }
+                string html = getHTML(signs[message]);
+                int horoscopeLocationInPTags = 3 + 3 * getDayOfWeek();
 
-                string horoscope = HttpUtility.HtmlDecode(days.InnerText);
+                string horoscope = dropHtmlPrecedingHoroscope(html, horoscopeLocationInPTags);
+                horoscope = dropHtmlTailingHoroscope(horoscope);
+                horoscope = html2Txt(horoscope);
+
                 Client.LocalUser.SendMessage(channel, horoscope);
             }
         }
 
+        private string html2Txt(string horoscope)
+        {
+            horoscope = horoscope.Replace("&auml;", "ä");
+            horoscope = horoscope.Replace("&ouml;", "ö");
+            return horoscope;
+        }
+
+        private string dropHtmlPrecedingHoroscope(string html, int pTagCount)
+        {
+            Regex pTagRegex = new Regex(@"(<p>)");
+            string[] htmlArray = pTagRegex.Split(html, pTagCount + 1);
+            string restOfHtml = htmlArray[pTagCount];
+
+            if (restOfHtml == null || restOfHtml.Length < 20)
+            {
+                return "Failed to parse HTML (horoscope too short)";
+            }
+
+            return restOfHtml;
+
+        }
+
+        private string dropHtmlTailingHoroscope(string html)
+        {
+            Regex pTagRegex2 = new Regex(@"(<br />)");
+            string[] horoscopefinal = pTagRegex2.Split(html, 0);
+            string result = horoscopefinal[0];
+
+            if (result == null || result.Length > 300)
+            {
+                return "Failed to parse HTML (tail after br-tag too long)";
+            }
+
+            return result;
+        }
+
         private int getDayOfWeek()
         {
-
             return (int)DateTime.Today.DayOfWeek + 1;
         }
 
-        private XmlDocument getXml(string sign)
+        private string getHTML(string sign)
         {
             string inputUrl = string.Format("http://www.astro.fi/future/weeklyForecast/sign/{0}", sign);
             var client = new WebClient();
             string content = client.DownloadString(inputUrl);
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(content);
-            return doc;
+            return content;
         }
         private string horoscopeHelpMsg()
         {
-            return "Usage: horoskooppi SIGN Where SIGN is the desired horoscope sign in finnish";
+            return "Usage: horos SIGN Where SIGN is the desired horoscope sign in Finnish (eg. neitsyt)";
         }
     }
 }
